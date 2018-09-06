63302c4d0d56ca850c1b3a93990686bfe7994ec0
shi.li_HSW-U+ <shi.li@united-imaging.com>
2018-09-06 14:09:31 +0800

    update style

--- Bandwidth test/jquery.bandwidth.js
+++ Bandwidth test/jquery.bandwidth.js
@@ -5,6 +5,7 @@
                 fileUrl: null,
                 fileSize: null,
                 domId: null,
+                bgColor: null
             }, options), duration, startTime, endTime, speedMbps;
 
             startTime = (new Date()).getTime();
@@ -15,38 +16,44 @@
                     endTime = (new Date()).getTime();
                     duration = (endTime - startTime) / 1000;
                     speedMbps = (stest.fileSize * 8 / duration / 1024).toFixed(2);
-                    var control = "<div id=\"signal-div\" style=\"position:relative; width:155px; height:80px; background-color: #fff;\" title=\""
+                    var control = "<div id=\"signal-div\" style=\"position:relative; width:140px; height:80px; background-color: " +
+                        stest.bgColor + ";\" title=\""
                         + speedMbps + "&nbsp; Mbps\">";
                     for (var i = 5; i > 0; i--) {
                         var height = (1 + i) * 10;
-                        var left = 10 + i * 20;
-                        var bgcolor = "gray";
+                        var left = i * 20 - 5;
+                        var bgcolor = "#494D50";
                         if ((i === 5 && speedMbps >= 40) || (i === 4 && speedMbps >= 20) ||
                         (i === 3 && speedMbps >= 10) || (i === 2 && speedMbps >= 5) ||
                         (i === 1 && speedMbps >= 2.5)) {
-                            bgcolor = "#090";
+                            bgcolor = "#D1DEE8";
                         }
-                        control += "<label id=\"signal-" + i + "\" style=\"width:10px; height:"
+                        control += "<label id=\"signal-" + i + "\" style=\"width:6px; height:"
                             + height + "px; position: absolute; background-color: " + bgcolor + "; left:"
-                            + left + "px; bottom:0; margin:0;\"></label>";
+                            + left + "px; bottom:0; margin:0; border-radius: 6px;\"></label>";
                     }
                     control += "</div>";
                     $("#" + stest.domId).html(control);
                 },
                 error: function (ex) {
-                    var errorHtml = "<div style=\"position: relative; width: 155px; height: 80px; background-color: #fff;\">\r\n" +
-                "        <span style=\"width: 40px; height: 40px; background-color: transparent; display: inline-block; position: absolute; left: 60px; bottom: -2px; z-index: 1; border-radius: 100%; text-align: center; line-height: 40px;\">\r\n" +
-                "            <label style=\"height: 40px; width: 10px; background-color: red; display: inline-block; transform: rotate(45deg);\">\r\n" +
-                "            </label>\r\n" +
-                "            <label style=\"height: 40px; width: 10px; background-color: red; display: inline-block; transform: rotate(-45deg); margin-left: -15px;\">\r\n" +
-                "            </label>\r\n" +
-                "        </span>\r\n" +
-                "        <label style=\"width: 10px; height: 60px; position: absolute; background-color: gray; left: 110px; bottom: 0; margin: 0;\"></label>\r\n" +
-                "        <label style=\"width: 10px; height: 50px; position: absolute; background-color: gray; left: 90px; bottom: 0; margin: 0;\"></label>\r\n" +
-                "        <label style=\"width: 10px; height: 40px; position: absolute; background-color: gray; left: 70px; bottom: 0; margin: 0;\"></label>\r\n" +
-                "        <label style=\"width: 10px; height: 30px; position: absolute; background-color: gray; left: 50px; bottom: 0; margin: 0;\"></label>\r\n" +
-                "        <label style=\"width: 10px; height: 20px; position: absolute; background-color: gray; left: 30px; bottom: 0; margin: 0;\"></label>\r\n" +
-                "    </div>";
+                    var errorHtml = "<div style=\"position:relative; width:140px; height:80px; background-color: " + stest.bgColor + ";\">";
+                    errorHtml += "<div style=\"width: 38px;height: 38px;position: absolute;background-color: #E4694F;z-index: 1;right: 14px;bottom: 0;border-radius: 100%;\">";
+                    errorHtml += "<label style=\"position: absolute;width: 32px;height: 32px;background-color: #0E0E0E;border-radius: 100%;left: 3px;top: 3px;text-align: center;line-height: 32px;color: #E4694F;font-size: 30px;\">!</label>";
+                    errorHtml += "</div>";
+                    for (var i = 0; i < 5; i++) {
+                        var custom = "bottom:0;";
+                        var backgroudColor = "#56626A";
+                        var left = 15 + i * 20;
+                        var height = 20 + i * 10;
+                        if (i === 4) {
+                            custom = "top: 20px;";
+                            backgroudColor = "#494D50";
+                            height = 20;
+                        }
+                        errorHtml += "<label style=\"width:6px;height:" + height + "px;position: absolute;background-color: " +
+                            backgroudColor + ";left:" + left + "px;" + custom + "margin:0;border-radius: 6px;\"></label>";
+                    }
+                    errorHtml += "</div>";
                     $("#" + stest.domId).html(errorHtml);
                 }
             });
